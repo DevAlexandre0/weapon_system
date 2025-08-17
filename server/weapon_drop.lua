e5fa5b31e9ed0e93bd402c1c279dd402f6774098
GESUS <105702323+DevAlexandre0@users.noreply.github.com>
2025-08-17 14:06:24 +0700

    Update weapon_drop.lua

--- server/weapon_drop.lua
+++ server/weapon_drop.lua
@@ -1,17 +1,12 @@
 local ox_inventory = exports["ox_inventory"]
 
 RegisterNetEvent('mbt_malisling:dropWeapon', function(data)
-    assert(data.hash ~= nil, 'dropWeapon ~ hash of weapons nil')
-
-    local r = ('DeadDrop %s000000000'):format(os.time(os.date('*t')))
-    
-    if type(data.slot) == 'number' then
-        local item = ox_inventory:GetSlot(source, data.slot)
-        local success = ox_inventory:RemoveItem(source, item.name, item.count, nil, item.slot)
-        if success then
-            ox_inventory:CustomDrop(r, {
-                { item.name, item.count, item.metadata }
-            }, GetEntityCoords(GetPlayerPed(source)), 1, 10000, nil, data.hash or `prop_water_corpse_01`)
-        end
+    if type(data.slot) ~= 'number' then return end
+    local item = ox_inventory:GetSlot(source, data.slot)
+    if not item then return end
+    if ox_inventory:RemoveItem(source, item.name, item.count, nil, item.slot) then
+        ox_inventory:CustomDrop(('DeadDrop %s000000000'):format(os.time()),
+            { { item.name, item.count, item.metadata } },
+            GetEntityCoords(GetPlayerPed(source)), 1, 10000, nil, data.hash or `prop_water_corpse_01`)
     end
 end)
